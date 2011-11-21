1926c0876e75477f7e0e7c0934679b8a2b679bb3
Skylar Hiebert <skylar.hiebert@computer.org>
2011-11-21 15:55:46 -0800

    Finished Add methods for QuadTree.

--- src/quadtree/QuadNode.java
+++ src/quadtree/QuadNode.java
@@ -31,9 +31,8 @@ public class QuadNode<T extends Body> {
 	/**
 	 * 
 	 */
-	public QuadNode(T entity, QuadNode<T> parent, Vector2D minVector, Vector2D maxVector) {
+	public QuadNode(QuadNode<T> parent, Vector2D minVector, Vector2D maxVector) {
 		this();
-		this.entities.add(entity);
 		this.parent = parent;
 		this.min = minVector;
 		this.max = maxVector;
@@ -43,7 +42,19 @@ public class QuadNode<T extends Body> {
 		if(entities.size() < MAX_ENTITIES)
 			entities.add(entity);
 		else {
-			
+			double minX = min.getX(), maxX = max.getX();
+			double minY = min.getY(), maxY = max.getY();
+			double midX = (maxX - minX) / 2, midY = (maxY - minY) / 2;
+			nwNode = new QuadNode<T>(this, new Vector2D(minX, minY), new Vector2D(midX, midY));
+			neNode = new QuadNode<T>(this, new Vector2D(midX, minY), new Vector2D(maxX, midY));
+			swNode = new QuadNode<T>(this, new Vector2D(minX, midY), new Vector2D(midX, maxY));
+			seNode = new QuadNode<T>(this, new Vector2D(midX, minY), new Vector2D(maxX, midY));
+			for(T ent : entities) {
+				double xPos = ent.getPosition().getX(), yPos = ent.getPosition().getY();
+				double width = ent.getWidth(), height = ent.getHeight();
+				if((xPos > minX && xPos < midX) && (yPos > minY && yPos < midY)) // Width and Height won't matter
+					;// nwNode
+			}
 		}
 	}
 	
@@ -63,7 +74,17 @@ public class QuadNode<T extends Body> {
 	
 	public List<T> getEntities(Vector2D min, Vector2D max) {
 		List<T> list = new ArrayList<T>();
-		if(entities != null)
+		double aMinX = min.getX(), aMaxX = max.getX(); 
+		double aMinY = min.getY(), aMaxY = max.getY();
+		double bMinX = this.min.getX(), bMaxX = this.max.getX(); 
+		double bMinY = this.min.getY(), bMaxY = this.max.getY();
+		if(entities != null &&
+				((aMinX > bMinX && aMinX < bMaxX) || // aMinX is inside b x-bounds
+						(aMaxX > bMinX && aMaxX < bMaxX) || // aMaxX is inside b x-bounds
+						(aMinX < bMinX && aMaxX > bMaxX)) && // aMinX and aMaxX surround b x-bounds
+				((aMinY > bMinY && aMinY < bMaxY) || // aMinY is inside b y-bounds
+								(aMaxY > bMinY && aMaxY < bMaxY) || // aMaxY is inside b Y-bounds
+								(aMinY < bMinY && aMaxY > bMaxY))) // aMinY and aMaxY surround b Y-bounds
 			list.addAll(entities);
 		if(nwNode != null)
 			list.addAll(nwNode.getEntities());
