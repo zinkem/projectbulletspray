731dd77f30f8e2e78a9e77de808bc859d67ba38f
Skylar Hiebert <skylar.hiebert@computer.org>
2011-11-30 00:10:27 -0800

    Completed QuadTree implementation (does not have delete Function). Verified ability to add and get entities using JUnit tests

--- src/quadtree/QuadNode.java
+++ src/quadtree/QuadNode.java
@@ -40,52 +40,51 @@ public class QuadNode<T extends Sprite> {
 	}
 	
 	public void addEntity(T entity) {
-		if(entities != null && entities.size() < maxEntities) {
-			entities.add(entity);
-		} else if (entities == null) {
+		if (entities == null) { /* Case: entities is full and has been made null, add to a child node */
 			double minX = min.getX(), maxX = max.getX();
 			double minY = min.getY(), maxY = max.getY();
-			double midX = (maxX - minX) / 2, midY = (maxY - minY) / 2;
+			double midX = minX + (maxX - minX) / 2, midY = minY + (maxY - minY) / 2;
 			double xPos = entity.getPosition().getX(), yPos = entity.getPosition().getY();
 			double width = entity.getWidth(), height = entity.getHeight();
-			System.out.println("xPos:" + xPos + " yPos:" + yPos + " minX,minY" + minX + "," + minY);
-			System.out.println("maxX,maxY" + maxX + "," + maxY + "  midX,midY" + midX + "," + midY);
 			if((xPos > minX && xPos < midX) && (yPos > minY && yPos < midY)) { // Width and Height won't matter
 				nwNode.addEntity(entity); // nwNode
 			} 
 			if(((xPos > midX || xPos + width > midX) && (xPos < maxX || xPos + width < maxX)) &&
 					((yPos > minY || yPos + height > minY) && (yPos < midY || yPos + height < midY))) {
-				neNode.addEntity(entity); //neNode
+				neNode.addEntity(entity); //neNode				
 			} 
 			if(((xPos > minX || xPos + width > minX) && (xPos < midX || xPos + width < midX)) &&
 					((yPos > midY || yPos + height > midY) && (yPos < maxY || yPos + height < maxY))) {
-				System.out.println("SW Entity:" + entity.getPosition());
 				swNode.addEntity(entity); //swNode
 			} 
 			if(((xPos > midX || xPos + width > midX) && (xPos < maxX || xPos + width < maxX)) &&
-					((yPos > minY || yPos + height > minY) && (yPos < maxY || yPos + height < maxY))) {
-				System.out.println("SE Entity:" + entity.getPosition());
+					((yPos > midY || yPos + height > midY) && (yPos < maxY || yPos + height < maxY))) {
 				seNode.addEntity(entity); //seNode
 			}
-		} else {
+			
+		} else if(entities.size() < maxEntities) { /* Case: entities is not full */ 
+			entities.add(entity);
+		} else  { /* Case: entities is full, create children nodes and move entities to children, make entities null */
 			double minX = min.getX(), maxX = max.getX();
 			double minY = min.getY(), maxY = max.getY();
-			double midX = (maxX - minX) / 2, midY = (maxY - minY) / 2;
+			double midX = minX + (maxX - minX) / 2, midY = minY + (maxY - minY) / 2;
 			nwNode = new QuadNode<T>(this, new Vector2D(minX, minY), new Vector2D(midX, midY));
 			neNode = new QuadNode<T>(this, new Vector2D(midX, minY), new Vector2D(maxX, midY));
 			swNode = new QuadNode<T>(this, new Vector2D(minX, midY), new Vector2D(midX, maxY));
-			seNode = new QuadNode<T>(this, new Vector2D(midX, minY), new Vector2D(maxX, midY));
+			seNode = new QuadNode<T>(this, new Vector2D(midX, midY), new Vector2D(maxX, maxY));
 			Iterator<T> iter = entities.iterator();
 			entities = null;
 			while(iter.hasNext()) {
 				addEntity(iter.next());
 			}
+			addEntity(entity);
 		}
 	}
 	
 	public List<T> getEntities() {
 		List<T> list = new ArrayList<T>();
-		list.addAll(entities);
+		if(entities != null)
+			list.addAll(entities);
 		if(nwNode != null)
 			list.addAll(nwNode.getEntities());
 		if(neNode != null)
@@ -108,21 +107,22 @@ public class QuadNode<T extends Sprite> {
 		double bMinX = this.min.getX(), bMaxX = this.max.getX(); 
 		double bMinY = this.min.getY(), bMaxY = this.max.getY();
 		if(entities != null &&
-				((aMinX > bMinX && aMinX < bMaxX) || // aMinX is inside b x-bounds
-						(aMaxX > bMinX && aMaxX < bMaxX) || // aMaxX is inside b x-bounds
-						(aMinX < bMinX && aMaxX > bMaxX)) && // aMinX and aMaxX surround b x-bounds
-				((aMinY > bMinY && aMinY < bMaxY) || // aMinY is inside b y-bounds
-								(aMaxY > bMinY && aMaxY < bMaxY) || // aMaxY is inside b Y-bounds
-								(aMinY < bMinY && aMaxY > bMaxY))) // aMinY and aMaxY surround b Y-bounds
+				((aMinX > bMinX && aMinX < bMaxX) || // aMinX is inside this x-bounds
+						(aMaxX > bMinX && aMaxX < bMaxX) || // aMaxX is inside this x-bounds
+						(aMinX < bMinX && aMaxX > bMaxX)) && // aMinX and aMaxX surround this x-bounds
+				((aMinY > bMinY && aMinY < bMaxY) || // aMinY is inside this y-bounds
+								(aMaxY > bMinY && aMaxY < bMaxY) || // aMaxY is inside this Y-bounds
+								(aMinY < bMinY && aMaxY > bMaxY))) { // aMinY and aMaxY surround this Y-bounds
 			list.addAll(entities);
+		}
 		if(nwNode != null)
-			list.addAll(nwNode.getEntities());
+			list.addAll(nwNode.getEntities(min, max));
 		if(neNode != null)
-			list.addAll(neNode.getEntities());
+			list.addAll(neNode.getEntities(min, max));
 		if(swNode != null)
-			list.addAll(swNode.getEntities());
+			list.addAll(swNode.getEntities(min, max));
 		if(seNode != null)
-			list.addAll(seNode.getEntities());
+			list.addAll(seNode.getEntities(min, max));
 		return list;
 	}
 	
