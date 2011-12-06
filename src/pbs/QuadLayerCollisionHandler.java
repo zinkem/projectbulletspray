90e5cb6da68680b2c9c830ac5267871d9a46d40e
Skylar Hiebert <skylar.hiebert@computer.org>
2011-12-05 19:45:26 -0800

    Corrected layer comparison for collision detection.

--- src/pbs/QuadLayerCollisionHandler.java
+++ src/pbs/QuadLayerCollisionHandler.java
@@ -3,7 +3,6 @@
  */
 package pbs;
 
-import jig.engine.physics.BodyLayer;
 import jig.engine.util.Vector2D;
 
 import java.awt.geom.Rectangle2D;
@@ -20,20 +19,8 @@ public abstract class QuadLayerCollisionHandler {
     
     public QuadLayerCollisionHandler(PBSQuadLayer<Entity> layer1, 
     		PBSQuadLayer<Entity> layer2){
-
-	//need to rewrite this, it's importan that layer1 corresponds to
-	//body 1, and layer2 corresponds to body2
-	//also, since layers change, we need to check this dynamically...
-	//so every time find & reconcile is executed
-
-    	/* Make the smaller layer, layer1 */
-    	if(layer1.size() < layer2.size()) {
-    		left = layer1;
-    		right = layer2;
-    	} else { 
-    		left = layer2;
-    		right = layer1;
-    	}
+    	left = layer1;
+    	right = layer2;
     }
     
     private Rectangle2D getBoundingBoxForBody(Entity body){
@@ -42,32 +29,44 @@ public abstract class QuadLayerCollisionHandler {
 				      body.getWidth()-1,body.getHeight()-1);
     }
 
-    //treat everything like a rectangle, n^2 algorithm
-    public void findAndReconcileCollisions(){
-
-	//layer size check should be here...
-	//and bodies passed to collide method
-	//need to correspond to layer order
-	
-	Rectangle2D bodyBoundingBox1 = null;
-	Rectangle2D bodyBoundingBox2 = null;
-	
-	for( Entity body1 : left ) {
-	    Vector2D b1Pos = body1.getPosition();
-	    for( Entity body2 : right.get(b1Pos, 
-					  new Vector2D(b1Pos.getX() + body1.getWidth(), 
-						       b1Pos.getY() + body1.getHeight())) ){
-		bodyBoundingBox1 = getBoundingBoxForBody(body1);
-		bodyBoundingBox2 = getBoundingBoxForBody(body2);
+    /* 
+     * Find the smallest layer and compare the layers
+     * Use the smaller layers bounding box as the min/max to
+     * get from the larger layers quad tree 
+     */
+    public void findAndReconcileCollisions(){	
+		Rectangle2D bodyBoundingBox1 = null;
+		Rectangle2D bodyBoundingBox2 = null;
+		PBSQuadLayer<Entity> inner, outer;
+		Boolean leftRight = true;
 		
-		if(bodyBoundingBox1 != null && bodyBoundingBox2 != null){
-		    if (bodyBoundingBox1.intersects(bodyBoundingBox2)) {
-			//System.out.println("collision!");
-			collide(body1, body2);
-		    }			    
+		if(left.size() < right.size()) {
+			outer = left; inner = right;			
+		} else {
+			outer = right; inner = left;
+			leftRight = false;
 		}
-	    }
-	}
+	
+		/* Make the smaller layer, the outer loop */
+		for( Entity body1 : outer ) {
+		    Vector2D b1Pos = body1.getPosition();
+		    for( Entity body2 : inner.get(b1Pos, 
+						  new Vector2D(b1Pos.getX() + body1.getWidth(), 
+							       b1Pos.getY() + body1.getHeight())) ){
+				bodyBoundingBox1 = getBoundingBoxForBody(body1);
+				bodyBoundingBox2 = getBoundingBoxForBody(body2);
+				
+				if(bodyBoundingBox1 != null && bodyBoundingBox2 != null){
+				    if (bodyBoundingBox1.intersects(bodyBoundingBox2)) {
+				    	//System.out.println("collision!");
+				    	if(leftRight) 
+				    		collide(body1, body2); // Body1 is layer1
+				    	else
+				    		collide(body2, body1); // Body2 is layer1
+				    }			    
+				}
+		    }
+		}
     }
     
     public abstract void collide(Entity body1, Entity body2);
