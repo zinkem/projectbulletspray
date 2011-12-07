e7d1c801cf9665d50bbeb36d7fd509e226ecd33b
Josh Roppo <joshroppo@gmail.com>
2011-12-06 20:07:53 -0800

    Added art to the spritesheet

--- src/pbs/Level.java
+++ src/pbs/Level.java
@@ -11,83 +11,99 @@ import pbs.Animations.*;
 
 public class Level {
 
+	public static final String SPRITE_SHEET = "resources/pbs-spritesheet.png";
 
-    public static final String SPRITE_SHEET = "resources/pbs-spritesheet.png";
-
-    public static enum Layer { 
-	BACKGROUND, 
-	    TRIGGERS,
-	    STATIC, 
-	    FX,
-	    HOSTILE, 
-	    ENEMY, 
-	    FRIENDLY, 
-	    PLAYER, 
-	    FOREGROUND,
-	    HUD }
-    
-    public static int NUM_LAYERS = Layer.values().length;
-    
-    //important level info
-    protected int score;
-    public int modScore(int m){ return score += m; }
-    public int getScore(){ return score; }
-    
-    protected int gametime;
-    public int getTime(){ return gametime/1000; }
-    
-    //center position of screen
-    protected Vector2D camera;
-    public void setCam(Vector2D cam){ camera = cam; }
-    public Vector2D getCam(){ return camera; }
-
-    //scroll vector
-    protected Vector2D scrollspeed;
-    public void setScrollSpeed(Vector2D ss){ scrollspeed = ss; }
-    public Vector2D getScrollSpeed(){ return scrollspeed; }
-
-    //list of layers
-    private List<PBSQuadLayer<Entity>> allTheLayers;
-
-    //list of collision handlers 
-    private List<QuadLayerCollisionHandler> collisionHandlers;
-
-    //statement list
-    private Stack<Statement> events;
-    public void addStatement(Statement s){ events.push(s); }
-    public void execute(){
-	Statement s;
-	while(!events.empty()){
-	    s = events.pop();
-	    s.execute(this);
+	public static enum Layer {
+		BACKGROUND, TRIGGERS, STATIC, FX, HOSTILE, ENEMY, FRIENDLY, PLAYER, FOREGROUND, HUD
 	}
-    }
 
-    //symbol table with templated entities/triggers
-    HashMap<String, ObjectDescription> templates;
-    public ObjectDescription getTemplate(String s){
-	System.out.println("get Template");
-	ObjectDescription od = templates.get(s);
-	if(od == null)
-	    System.out.println("No object description named " + s);
-	return od;
-    }
-    public void addTemplate(String s, ObjectDescription od){
-	templates.put(s, od);
-    }
+	public static int NUM_LAYERS = Layer.values().length;
 
+	// important level info
+	protected int score;
 
-    public Level(){
-	score = 0;
-	gametime = 0;
-	
-	camera = new Vector2D(320,240);
-	scrollspeed = new Vector2D(1, 0);
-	
-	events = new Stack<Statement>();
-	templates = new HashMap<String, ObjectDescription>();
-    }
+	public int modScore(int m) {
+		return score += m;
+	}
 
+	public int getScore() {
+		return score;
+	}
+
+	protected int gametime;
+
+	public int getTime() {
+		return gametime / 1000;
+	}
+
+	// center position of screen
+	protected Vector2D camera;
+
+	public void setCam(Vector2D cam) {
+		camera = cam;
+	}
+
+	public Vector2D getCam() {
+		return camera;
+	}
+
+	// scroll vector
+	protected Vector2D scrollspeed;
+
+	public void setScrollSpeed(Vector2D ss) {
+		scrollspeed = ss;
+	}
+
+	public Vector2D getScrollSpeed() {
+		return scrollspeed;
+	}
+
+	// list of layers
+	private List<PBSQuadLayer<Entity>> allTheLayers;
+
+	// list of collision handlers
+	private List<QuadLayerCollisionHandler> collisionHandlers;
+
+	// statement list
+	private Stack<Statement> events;
+
+	public void addStatement(Statement s) {
+		events.push(s);
+	}
+
+	public void execute() {
+		Statement s;
+		while (!events.empty()) {
+			s = events.pop();
+			s.execute(this);
+		}
+	}
+
+	// symbol table with templated entities/triggers
+	HashMap<String, ObjectDescription> templates;
+
+	public ObjectDescription getTemplate(String s) {
+		System.out.println("get Template");
+		ObjectDescription od = templates.get(s);
+		if (od == null)
+			System.out.println("No object description named " + s);
+		return od;
+	}
+
+	public void addTemplate(String s, ObjectDescription od) {
+		templates.put(s, od);
+	}
+
+	public Level() {
+		score = 0;
+		gametime = 0;
+
+		camera = new Vector2D(320, 240);
+		scrollspeed = new Vector2D(1, 0);
+
+		events = new Stack<Statement>();
+		templates = new HashMap<String, ObjectDescription>();
+	}
 
 	// setters
 	public void add(Entity e, Layer l) {
@@ -125,8 +141,6 @@ public class Level {
 					add(e, Layer.FX);
 					body1.kill();
 					body2.modhp(-1);
-					
-					
 
 					if (body2.alive() == false) {
 						score += body2.value();
@@ -145,50 +159,55 @@ public class Level {
 				}
 			}
 		});
-	collisionHandlers.add(new QuadLayerCollisionHandler(getLayer(Layer.PLAYER), getLayer(Layer.STATIC)){
+		collisionHandlers.add(new QuadLayerCollisionHandler(
+				getLayer(Layer.PLAYER), getLayer(Layer.STATIC)) {
 
-		@Override
-		public void collide(Entity body1, Entity body2) {
-			if(body1.isActive() && body2.isActive()){
-				double rx = 4.0*Math.random() -2.0;
-				double ry = 4.0*Math.random() -2.0;
-				
-				Entity e = new Entity(SPRITE_SHEET + "#large_burst");
-				e.setPosition(body1.getCenterPosition().translate(new Vector2D(rx, ry)));
-				e.setCustomAnimation(new AnimateOnce(75));
-				add(e, Layer.FX);
-				
-				//Kill the player, do any sort of updates to the game necessary
-				body1.kill();
-				
-				
-			}
-		}
-	});
-	
-	collisionHandlers.add(new QuadLayerCollisionHandler(getLayer(Layer.PLAYER), getLayer(Layer.ENEMY)){
+			@Override
+			public void collide(Entity body1, Entity body2) {
+				if (body1.isActive() && body2.isActive()) {
+					double rx = 4.0 * Math.random() - 2.0;
+					double ry = 4.0 * Math.random() - 2.0;
 
-		@Override
-		public void collide(Entity body1, Entity body2) {
-			if(body1.isActive() && body2.isActive()){
-				double rx = 8.0*Math.random() -4.0;
-				double ry = 8.0*Math.random() -4.0;
-				
-				Entity e = new Entity(SPRITE_SHEET + "#large_burst");
-				e.setPosition(body1.getCenterPosition().translate(new Vector2D(rx, ry)));
-				e.setCustomAnimation(new AnimateOnce(75));
-				add(e, Layer.FX);
-				
-				e = new Entity(SPRITE_SHEET + "#small_burst");
-				e.setPosition(body2.getCenterPosition().translate(new Vector2D(ry, rx)));
-				e.setCustomAnimation(new AnimateOnce(75));
-				add(e, Layer.FX);
-				
-				body2.kill();
-				body1.modhp(-1);
+					Entity e = new Entity(SPRITE_SHEET + "#large_burst");
+					e.setPosition(body1.getCenterPosition().translate(
+							new Vector2D(rx, ry)));
+					e.setCustomAnimation(new AnimateOnce(75));
+					add(e, Layer.FX);
+
+					// Kill the player, do any sort of updates to the game
+					// necessary
+					body1.kill();
+
+				}
 			}
-		}
-	});
+		});
+
+		collisionHandlers.add(new QuadLayerCollisionHandler(
+				getLayer(Layer.PLAYER), getLayer(Layer.ENEMY)) {
+
+			@Override
+			public void collide(Entity body1, Entity body2) {
+				if (body1.isActive() && body2.isActive()) {
+					double rx = 8.0 * Math.random() - 4.0;
+					double ry = 8.0 * Math.random() - 4.0;
+
+					Entity e = new Entity(SPRITE_SHEET + "#large_burst");
+					e.setPosition(body1.getCenterPosition().translate(
+							new Vector2D(rx, ry)));
+					e.setCustomAnimation(new AnimateOnce(75));
+					add(e, Layer.FX);
+
+					e = new Entity(SPRITE_SHEET + "#small_burst");
+					e.setPosition(body2.getCenterPosition().translate(
+							new Vector2D(ry, rx)));
+					e.setCustomAnimation(new AnimateOnce(75));
+					add(e, Layer.FX);
+
+					body2.kill();
+					body1.modhp(-1);
+				}
+			}
+		});
 
 		collisionHandlers.add(new ElasticCollisionHandler(
 				getLayer(Layer.ENEMY), getLayer(Layer.PLAYER)));
@@ -232,27 +251,27 @@ public class Level {
 		// check for collisions
 		checkForCollisions();
 
-	//behavior custom to each level goes here
+		// behavior custom to each level goes here
 
-	//check to see which triggers need to be fired...
-	Iterator<Entity> elist = getLayer(Layer.TRIGGERS).iterator();
-	if(elist != null)
-	    while(elist.hasNext()){
-		Entity t = elist.next();
-		t.fireTrigger(this, deltaMs);
-	    }
-	
-	//call "shoot" on all enemy entities in case they have weapons to fire
-	elist = getLayer(Layer.ENEMY).iterator();
-	if(elist != null)
-	    while(elist.hasNext()){
-		Entity e = elist.next();
-		e.shoot(this, deltaMs);
-	    }
+		// check to see which triggers need to be fired...
+		Iterator<Entity> elist = getLayer(Layer.TRIGGERS).iterator();
+		if (elist != null)
+			while (elist.hasNext()) {
+				Entity t = elist.next();
+				t.fireTrigger(this, deltaMs);
+			}
 
-	//check for collisions
-	checkForCollisions();	
-	
-    }
+		// call "shoot" on all enemy entities in case they have weapons to fire
+		elist = getLayer(Layer.ENEMY).iterator();
+		if (elist != null)
+			while (elist.hasNext()) {
+				Entity e = elist.next();
+				e.shoot(this, deltaMs);
+			}
+
+		// check for collisions
+		checkForCollisions();
+
+	}
 
 }
